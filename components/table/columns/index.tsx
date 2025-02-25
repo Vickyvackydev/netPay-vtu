d141e73011d1e8a756a1e0f996d16392ccf34d5d
Vickyvackydev <vickyvacky5@gmail.com>
2025-02-25 01:01:04 -0800

    bug fix

--- components/table/columns/index.tsx
+++ components/table/columns/index.tsx
@@ -1,97 +1,90 @@
 "use client";
 
 import Image from "next/image";
+import { createColumnHelper } from "@tanstack/react-table";
+
+const columnHelper = createColumnHelper<any>();
 
 export const TRANSACTION_COLUMN = [
-  {
-    Header: "Transaction",
-    accessor: "transaction",
-    Cell: ({ row }: { row: any }) => {
-      return (
-        <div className="flex items-center justify-start gap-x-6">
-          {row?.original?.status === "successful" ? (
-            <Image
-              src={"./images/sucess.svg"}
-              width={32}
-              height={32}
-              className="rounded-full object-contain"
-              alt="sucess"
-            />
-          ) : (
-            <Image
-              src={"./images/unsucess.svg"}
-              width={32}
-              height={32}
-              className="rounded-full object-contain"
-              alt="sucess"
-            />
-          )}
-          <span className="text-sm font-medium text-defaultBlack">
-            {row?.original?.transaction_detail}
-          </span>
-        </div>
-      );
-    },
-  },
-  {
-    Header: "Date",
-    accessor: "date",
-    Cell: ({ row }: { row: any }) => (
+  columnHelper.accessor("transaction", {
+    header: "Transaction",
+    cell: ({ row }) => (
+      <div className="flex items-center justify-start gap-x-6">
+        {row.original.status === "successful" ? (
+          <Image
+            src={"./images/sucess.svg"}
+            width={32}
+            height={32}
+            className="rounded-full object-contain"
+            alt="success"
+          />
+        ) : (
+          <Image
+            src={"./images/unsucess.svg"}
+            width={32}
+            height={32}
+            className="rounded-full object-contain"
+            alt="unsuccessful"
+          />
+        )}
+        <span className="text-sm font-medium text-defaultBlack">
+          {row.original.transaction_detail}
+        </span>
+      </div>
+    ),
+  }),
+  columnHelper.accessor("date", {
+    header: "Date",
+    cell: () => (
       <span className="text-sm font-normal text-[#4B5563]">
         {`${new Date().getDate()}/${
           new Date().getMonth() + 1
         }/${new Date().getFullYear()}`}
       </span>
     ),
-  },
-  {
-    Header: "Time",
-    accessor: "time",
-    Cell: ({ row }: { row: any }) => (
+  }),
+  columnHelper.accessor("time", {
+    header: "Time",
+    cell: () => (
       <span className="text-sm font-medium text-[#111827]">10:12 pm</span>
     ),
-  },
-  {
-    Header: "Status",
-    accessor: "status",
-    Cell: ({ row }: { row: any }) => {
-      return (
-        <span
-          className={`${
-            row?.original?.status === "successful"
-              ? "text-[#008080]"
-              : "text-[#D10019]"
-          }`}
-        >
-          {row?.original?.status}
-        </span>
-      );
-    },
-  },
-  {
-    Header: "Type",
-    accessor: "type",
-    Cell: ({ row }: { row: any }) => (
-      <span className="text-sm font-medium text-[#111827]">
-        {row?.original?.transaction_type}
+  }),
+  columnHelper.accessor("status", {
+    header: "Status",
+    cell: ({ row }) => (
+      <span
+        className={
+          row.original.status === "successful"
+            ? "text-[#008080]"
+            : "text-[#D10019]"
+        }
+      >
+        {row.original.status}
       </span>
     ),
-  },
-  {
-    Header: "Amount",
-    accessor: "amount",
-    Cell: ({ row }: { row: any }) => (
+  }),
+  columnHelper.accessor("type", {
+    header: "Type",
+    cell: ({ row }) => (
+      <span className="text-sm font-medium text-[#111827]">
+        {row.original.transaction_type}
+      </span>
+    ),
+  }),
+  columnHelper.accessor("amount", {
+    header: "Amount",
+    cell: ({ row }) => (
       <span
         className={`text-xl font-medium ${
-          row?.original?.action_type === "sent"
+          row.original.action_type === "sent"
             ? "text-[#008080]"
             : "text-[#D10019]"
         }`}
       >
-        {row?.original?.action_type === "sent"
-          ? `+${row?.original?.amount}`
-          : `-${row?.original?.amount}`}
+        {row.original.action_type === "sent"
+          ? `+${row.original.amount}`
+          : `-${row.original.amount}`}
       </span>
     ),
-  },
+  }),
 ];
--- components/table/index.tsx
+++ components/table/index.tsx
@@ -1,8 +1,13 @@
 "use client";
 
-import React, { useMemo } from "react";
+import React from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import { useTable, usePagination, Column } from "react-table";
+import {
+  useReactTable,
+  getCoreRowModel,
+  getPaginationRowModel,
+  flexRender,
+} from "@tanstack/react-table";
 
 interface TableData {
   [key: string]: any;
@@ -10,174 +15,88 @@ interface TableData {
 
 interface TableProps {
   DATA: TableData[];
-  COLUMNS: Column<TableData>[];
+  COLUMNS: any[];
 }
 
 function TableComponent({ DATA, COLUMNS }: TableProps) {
-  const columns = useMemo(() => COLUMNS, [COLUMNS]);
-  const data = useMemo(() => DATA, [DATA]);
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-    // @ts-ignore
-    page, // For pagination
-    // @ts-ignore
-    canPreviousPage,
-    // @ts-ignore
-    canNextPage,
-    // @ts-ignore
-    pageOptions,
-    // @ts-ignore
-    gotoPage,
-    // @ts-ignore
-    nextPage,
-    // @ts-ignore
-    previousPage,
-    // @ts-ignore
-    setPageSize,
-    // @ts-ignore
-    state: { pageIndex, pageSize },
-  } = useTable(
-    {
-      columns,
-      data,
-      // @ts-ignore
-      initialState: { pageIndex: 0 }, // Start from page 0
-    },
-    usePagination
-  );
+  const table = useReactTable({
+    data: DATA,
+    columns: COLUMNS,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
 
   return (
     <div className="w-full">
       {/* Table */}
-      <table
-        {...getTableProps()}
-        className="w-full table-auto border-collapse text-right"
-      >
+      <table className="w-full table-auto border-collapse text-right">
         <thead className="w-full text-start">
-          {headerGroups.map((headerGroup: any, index: number) => (
-            <tr
-              key={index}
-              {...headerGroup.getHeaderGroupProps()}
-              className="border-b border-[#E5E7EB]"
-            >
-              {headerGroup.headers.map((column: any) => (
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id} className="border-b border-[#E5E7EB]">
+              {headerGroup.headers.map((header) => (
                 <th
-                  {...column.getHeaderProps()}
+                  key={header.id}
                   className="whitespace-nowrap pb-2 pl-6 pr-3 text-left text-sm font-semibold text-primary-default"
                 >
-                  {column.render("Header")}
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
                 </th>
               ))}
             </tr>
           ))}
         </thead>
-        <tbody {...getTableBodyProps()} className="px-8 ">
-          {page.map((row: any, index: number) => {
-            prepareRow(row);
-            return (
-              <tr
-                key={index}
-                {...row.getRowProps()}
-                className="border-b border-[#E5E7EB] text-left"
-              >
-                {row.cells.map((cell: any) => (
-                  <td
-                    {...cell.getCellProps()}
-                    className="py-5 pl-6 pr-3 text-sm text-gray-700"
-                  >
-                    {cell.render("Cell")}
-                  </td>
-                ))}
-              </tr>
-            );
-          })}
+        <tbody className="px-8">
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id} className="border-b border-[#E5E7EB] text-left">
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  key={cell.id}
+                  className="py-5 pl-6 pr-3 text-sm text-gray-700"
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
 
       {/* Pagination */}
-      {/* <div className="">
-        <span className="flex items-center gap-x-1 text-[14px] font-medium text-[#344054]">
-          Page{" "}
-          <span className="flex h-[27px] w-[27px] items-center justify-center rounded-md border border-[#D0D5DD]">
-            {pageIndex + 1}
-          </span>
-          of
-          <span>{pageOptions.length}</span>
-        </span>
-        <div>
-          <button
-            onClick={() => previousPage()}
-            disabled={!canPreviousPage}
-            className="mr-2 rounded border border-[#D0D5DD] px-4 py-2 text-[14px] font-normal text-[#344054] disabled:opacity-50"
-          >
-            Previous
-          </button>
-          <button
-            onClick={() => nextPage()}
-            disabled={!canNextPage}
-            className="mr-2 rounded border border-[#D0D5DD] px-4 py-2 text-[14px] font-normal text-[#344054] disabled:opacity-50"
-          >
-            Next
-          </button>
-        </div>
-        <div>
-          <label>
-            <span className="text-[14px] font-medium text-[#344054]">
-              Entries per page:{" "}
-            </span>
-            <select
-              value={pageSize}
-              onChange={(e) => setPageSize(Number(e.target.value))}
-              className="ml-2 rounded border px-2 py-1"
-            >
-              {[10, 20, 30, 40, 50].map((pageSize) => (
-                <option key={pageSize} value={pageSize}>
-                  {pageSize}
-                </option>
-              ))}
-            </select>
-          </label>
-        </div>
-      </div> */}
-      {pageOptions?.length > 5 && (
+      {table.getPageCount() > 1 && (
         <div className="mt-8 flex w-full items-center justify-between">
           <button
-            onClick={() => previousPage()}
-            disabled={!canPreviousPage}
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
             className="flex h-[36px] w-[100px] items-center justify-center rounded-md border border-[#E5E7EB] bg-[#F9FAFB] text-sm font-normal text-[#111827]"
           >
-            {/* <img src={CHEVRON_LEFT} className="h-[20px] w-[20px]" alt="" /> */}
             <FaChevronLeft size={20} />
             <span>Previous</span>
           </button>
           <div className="flex items-center gap-x-2">
-            {pageOptions.map((_: any, index: number) => (
+            {Array.from({ length: table.getPageCount() }, (_, index) => (
               <button
                 key={index}
-                className="flex h-[36px] w-[36px] items-center justify-center rounded-md border border-[#E5E7EB] bg-[#F9FAFB]"
+                onClick={() => table.setPageIndex(index)}
+                className={`flex h-[36px] w-[36px] items-center justify-center rounded-md border border-[#E5E7EB] bg-[#F9FAFB] ${
+                  table.getState().pagination.pageIndex === index
+                    ? "bg-gray-300"
+                    : ""
+                }`}
               >
-                <span className="text-sm text-primary-default">{`0${
-                  index + 1
-                }`}</span>
+                <span className="text-sm text-primary-default">
+                  {index + 1}
+                </span>
               </button>
             ))}
           </div>
           <button
-            onClick={() => nextPage()}
-            disabled={!canNextPage}
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
             className="flex h-[36px] w-[100px] items-center justify-center rounded-md border border-[#E5E7EB] bg-[#F9FAFB] text-sm font-normal text-[#111827]"
           >
             <span>Next</span>
-            {/* <img
-              src={CHEVRON_LEFT}
-              className="h-[20px] w-[20px] scale-x-[-1]"
-              alt=""
-            /> */}
             <FaChevronRight size={20} />
           </button>
         </div>

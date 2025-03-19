b334c9bc14a21f9c5d3c1acdf2c3f7925b1b9abd
Vickyvackydev <vickyvacky5@gmail.com>
2025-03-19 09:51:56 -0400

    update

--- layouts/AuthLayout.tsx
+++ layouts/AuthLayout.tsx
@@ -3,18 +3,10 @@ import React from "react";
 
 function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      className="w-full flex items-center justify-center"
-      style={{
-        background: "url('./images/yellow-background.png')",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        height: "100vh",
-      }}
-    >
+    <div className="w-full min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-100 to-gray-200">
       <div className="flex items-center justify-center flex-col gap-y-6">
-        <Logo size={200} />
-        <div className="w-[441px] h-full p-10 bg-white rounded-xl shadow-sm">
+        <Logo size={150} />
+        <div className="w-[450px] h-full p-10 bg-white rounded-2xl shadow-lg border border-gray-200">
           {children}
         </div>
       </div>
